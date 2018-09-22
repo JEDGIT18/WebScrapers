db1fa5ffbbd584ddc3c2db01eb3390dd29257f8f
Joshua Drakes <joshuaedrakes@gmail.com>
2018-09-21 21:28:45 -0400

    Add files via upload

--- EbaySpider.py
+++ EbaySpider.py
@@ -76,18 +76,6 @@ numlines is an optional argument used only as a fall-back.
   else:
     # Fallback for other operating systems.
     print('\n' * numlines)
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   BOLD = '\033[1m'
-   UNDERLINE = '\033[4m'
-   END = '\033[0m'
-
 tradeSpiderEbay(maxPages)
 winsound.Beep(400, 500)
 
